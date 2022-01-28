47c5492974a57dd8e51e924905f5ecf2a81eb95c
verytallman <oleksiidehtiarov@gmail.com>
2022-01-28 14:24:30 +0200

    Working on HW20 - task 2 done - task 3 done - task * done

--- src/test/java/org/homework/Task2.java
+++ src/test/java/org/homework/Task2.java
@@ -13,11 +13,11 @@ public class Task2 extends BaseTestHomeWork {
         webHelpers.scrollToElement(webDriver, aboutGurockLink);
         aboutGurockLink.click();
         WebElement desiredText1 = webDriver.findElement(By.xpath("//h2[contains(text(),'Outstanding customer satisfaction')]"));
-        Assert.assertEquals(webDriver.findElement(By.xpath("//h2[contains(text(),'Outstanding customer satisfaction')]")),
-                desiredText1);
+        Assert.assertEquals(webDriver.findElement(By.xpath("//h2[contains(text(),'Outstanding customer satisfaction')]")).isDisplayed(),
+                desiredText1.isDisplayed());
         WebElement desiredText2 = webDriver.findElement(By.xpath("//a[contains(text(),'2021 G2 Grid for Test Management and Software Test')]"));
-        Assert.assertEquals(webDriver.findElement(By.xpath("//a[contains(text(),'2021 G2 Grid for Test Management and Software Test')]")),
-                desiredText2);
+        Assert.assertEquals(webDriver.findElement(By.xpath("//a[contains(text(),'2021 G2 Grid for Test Management and Software Test')]")).isDisplayed(),
+                desiredText2.isDisplayed());
     }
 
     @Test
@@ -27,11 +27,11 @@ public class Task2 extends BaseTestHomeWork {
         webHelpers.scrollToElement(webDriver, jobsLink);
         jobsLink.click();
         WebElement desiredText1 = webDriver.findElement(By.xpath("//h2[contains(text(),'Browse Open Positions')]"));
-        Assert.assertEquals(webDriver.findElement(By.xpath("//h2[contains(text(),'Browse Open Positions')]")),
-                desiredText1);
+        Assert.assertEquals(webDriver.findElement(By.xpath("//h2[contains(text(),'Browse Open Positions')]")).isDisplayed(),
+                desiredText1.isDisplayed());
         WebElement browseOpenButton = webDriver.findElement(By.xpath("//a[@class='btn btn-medium btn-outline-blue']"));
-        Assert.assertEquals(webDriver.findElement(By.xpath("//a[@class='btn btn-medium btn-outline-blue']")),
-                browseOpenButton);
+        Assert.assertEquals(webDriver.findElement(By.xpath("//a[@class='btn btn-medium btn-outline-blue']")).isDisplayed(),
+                browseOpenButton.isDisplayed());
     }
 
     @Test
@@ -42,11 +42,11 @@ public class Task2 extends BaseTestHomeWork {
         webHelpers.scrollToElement(webDriver, contactLink);
         contactLink.click();
         WebElement desiredTelephoneNumber = webDriver.findElement(By.xpath("//span[contains(text(),' +49-6103-599-6099')]"));
-        Assert.assertEquals(webDriver.findElement(By.xpath("//span[contains(text(),' +49-6103-599-6099')]")),
-                desiredTelephoneNumber);
+        Assert.assertEquals(webDriver.findElement(By.xpath("//span[contains(text(),' +49-6103-599-6099')]")).isDisplayed(),
+                desiredTelephoneNumber.isDisplayed());
         WebElement desiredText = webDriver.findElement(By.xpath("//span[contains(text(),'HRB53083 Offenbach am Main')]"));
-        Assert.assertEquals(webDriver.findElement(By.xpath("//span[contains(text(),'HRB53083 Offenbach am Main')]")),
-                desiredText);
+        Assert.assertEquals(webDriver.findElement(By.xpath("//span[contains(text(),'HRB53083 Offenbach am Main')]")).isDisplayed(),
+                desiredText.isDisplayed());
     }
 
     @Test
